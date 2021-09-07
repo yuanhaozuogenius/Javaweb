602627da31380b96587e2da772fe02467e4c7893
“yuanhaozuogenius” <yuanhaozuogenius@gmail.com>
2021-09-07 14:59:02 +0800

    simple JAVA calculator

---
+++ Simple-calculator-with-Spring-MVC/src/main/java/com/softuni/models/Calculator.java
@@ -0,0 +1,69 @@
+package com.softuni.models;
+
+/**
+ * Demo class
+ *
+ * @author Barry
+ * @date 2021/09/07
+ */
+@SuppressWarnings("AlibabaClassMustHaveAuthor")
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public Calculator(double leftOperand, double rightOperand, String operator) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+        this.operator = operator;
+    }
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public double calculateResult() {
+        double result = 0;
+        switch(this.operator) {
+            case "+":
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case "-":
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case "*":
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case "/":
+                result = this.leftOperand / this.rightOperand;
+                break;
+            case "^":
+                result = Math.pow(this.leftOperand,this.rightOperand);
+                break;
+            default:
+                    result = 0;
+        }
+
+        return result;
+    }
+}
